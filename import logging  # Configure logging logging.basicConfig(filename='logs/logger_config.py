744a3afb36f606333c8c62dd1254ac04a82e5a67
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-05 13:47:17 +0530

    Delete import logging  # Configure logging logging.basicConfig(filename='logs/logger_config.py

--- import logging  # Configure logging logging.basicConfig(filename='logs/logger_config.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-# Configure logging
-logging.basicConfig(filename='logs/project.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s')
