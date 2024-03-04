15cbaa8a7e2a1d47bc40303c33bce23c942bee37
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-03-04 14:18:25 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,44 +1,36 @@
 import unittest
 from pathlib import Path
+
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from model import Model  # Adjust the import path as needed
 
-# Define the path to the model
+from src.model import ModelHandler
+
 PROJECT_ROOT = Path(__file__).parent.parent
 MODEL_PATH = PROJECT_ROOT / "models/model.joblib"
 
+
 class TestModel(unittest.TestCase):
     """
-    Test cases for verifying the functionality of the Model class.
+    Unit tests for the ModelHandler class.
     """
-
-    def setUp(self):
-        """
-        Set up a Model instance for testing.
-        """
-        # Initialize the Model with the path to the model file
-        self.model = Model(model_path=str(MODEL_PATH))
 
     def test_load_model(self):
         """
-        Test loading the machine learning model.
+        Test whether the load_model method loads the model correctly.
         """
-        # The model is already loaded in setUp
-        self.assertIsInstance(self.model.model, RandomForestRegressor)
+        model = ModelHandler().load_model(str(MODEL_PATH))
+        self.assertIsInstance(model, RandomForestRegressor)
 
     def test_predict(self):
         """
-        Test making predictions with the model.
+        Test whether the predict method makes predictions using the loaded model.
         """
-        # Create a test input DataFrame
+        model = ModelHandler().load_model(str(MODEL_PATH))
         test_input = pd.DataFrame([[0] * 13], columns=[f'feature_{i}' for i in range(13)])
-        
-        # Make a prediction using the model
-        prediction = self.model.predict(test_input)
-        
-        # Check if the prediction is successful (i.e., prediction length > 0)
+        prediction = ModelHandler().predict(test_input, model)
         self.assertTrue(len(prediction) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
