6bcb30cbb9334113d07879dd2f08b7d3dabae1ef
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-03-04 14:15:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,112 @@
 import logging
-from pathlib import Path
+import sys
+
+import pandas as pd
 from sklearn.metrics import mean_absolute_error
-from database import DatabaseManager  # Adjust the import path as needed
-from model import Model  # Adjust the import path as needed
-from etl import ETLProcessor  # Adjust the import path as needed
-from predictor import Predictor  # Adjust the import path as needed
-from logger_config import setup_logger  # Ensure this is correctly set up
 
-# Initialize the logger
-setup_logger()
+from constants import TRAIN_DATA_PATH, MODEL_PATH
+from src.database import DatabaseHandler
+from src.etl import DataProcessor
+from src.model import ModelHandler
+from src.predictor import PredictionRunner
 
-def main() -> None:
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+class DataPipeline:
     """
-    Main function to orchestrate the data engineering pipeline including data preparation,
-    model training, prediction, and evaluation.
+    Class to run the data engineering pipeline.
+    It includes data preparation, model loading, prediction, and evaluation.
     """
-    try:
-        # Define file paths
-        project_root = Path(__file__).parent
-        model_path = project_root / "models/model.joblib"
-        train_data_path = project_root / "data/housing.csv"
 
-        # Initialize database manager
-        db_manager = DatabaseManager(database_name="housing_data.db")
-        
-        # Initialize and load model
-        model = Model(model_path=str(model_path))
-        
-        # Process data using ETL
-        etl_processor = ETLProcessor(data_path=str(train_data_path))
-        X_train, X_test, y_train, y_test = etl_processor.prepare_data()
-        
-        # Train the model or load if already trained
-        if not model.model:  # Assuming the load_model sets model to None if it fails
-            model.train(X_train, y_train)
-            model.save_model(model.model)
-        
-        # Make predictions on the test set
-        predictor = Predictor(model=model)
-        predicted_values = predictor.perform_prediction(db_manager, X_test)
-        logging.info(f"Predictions: {predicted_values}")
+    def __init__(self):
+        """
+        Initialize DataPipeline object.
+        """
+        self.model = None
+        self.conn = None
 
-        # Evaluate model performance
-        train_error = mean_absolute_error(y_train, model.predict(X_train))
-        test_error = mean_absolute_error(y_test, predicted_values)
+    def prepare_data(self):
+        """
+        Prepare the data for training and testing.
+
+        Returns:
+            Tuple: X_train, X_test, y_train, y_test
+        """
+        self.conn = DatabaseHandler().create_database()
+        return DataProcessor().prepare_data(str(TRAIN_DATA_PATH))
+
+    def load_model(self):
+        """
+        Load the trained machine learning model.
+
+        Returns:
+            model: Trained machine learning model
+        """
+        return ModelHandler().load_model(str(MODEL_PATH))
+
+    def evaluate_model(self, y_train, y_test, y_pred_train, y_pred_test):
+        """
+        Evaluate the trained model.
+
+        Args:
+            y_train (array-like): True values of the target variable for the training set.
+            y_test (array-like): True values of the target variable for the test set.
+            y_pred_train (array-like): Predicted values for the training set.
+            y_pred_test (array-like): Predicted values for the test set.
+        """
+        train_error = mean_absolute_error(y_train, y_pred_train)
+        test_error = mean_absolute_error(y_test, y_pred_test)
         logging.info(f'Train error: {train_error}')
         logging.info(f'Test error: {test_error}')
 
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    finally:
-        logging.info('Closing database connection.')
-        # Ensure the connection is closed properly
-        if db_manager and db_manager.connection:
-            db_manager.connection.close()
+    def perform_prediction(self, data):
+        """
+        Perform predictions on the provided data and save the results to the database.
+
+        Args:
+            data (DataFrame): Data to perform predictions on.
+
+        Returns:
+            DataFrame: Predicted values.
+        """
+        predictions = ModelHandler().predict(data, self.model)
+        predictions_df = pd.DataFrame(predictions, columns=['Predicted_Value'])
+        DatabaseHandler().save_predictions_to_database(predictions_df)
+        return predictions_df
+
+    def run_pipeline(self):
+        """
+        Run the data engineering pipeline.
+        """
+        try:
+            X_train, X_test, y_train, y_test = self.prepare_data()
+
+            logging.info('Loading the model...')
+            self.model = self.load_model()
+
+            logging.info('Calculating train dataset predictions...')
+            y_pred_train = ModelHandler().predict(X_train, self.model)
+            y_pred_test = ModelHandler().predict(X_test, self.model)
+
+            logging.info('Evaluating the model...')
+            self.evaluate_model(y_train, y_test, y_pred_train, y_pred_test)
+
+            logging.info('Performing predictions...')
+            df = DatabaseHandler().load_from_database()
+            predicted_values = self.perform_prediction(df)
+            logging.info(predicted_values[:5])
+
+            predictions_list = PredictionRunner().run_prediction(str(MODEL_PATH))
+            logging.info(predictions_list)
+
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+        finally:
+            logging.info('Closing database connection.')
+            self.conn.close()
+
 
 if __name__ == '__main__':
-    main()
+    pipeline = DataPipeline()
+    pipeline.run_pipeline()
